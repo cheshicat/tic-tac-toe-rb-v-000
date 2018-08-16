ecd71958a57c006b74026910e02b47967ac5a87b
Allison Howard <alliemhow@gmail.com>
2018-08-16 19:38:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,18 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+   puts "Congratulations #{winner(board)}!"
+ elsif draw?(board)
+   puts "Cat's Game!"
+ end
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -17,46 +29,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-   user_input.to_i - 1
-end
-
-def move(board, index, player)
-  board[index] = player
-end
-
-def position_taken?(board, index)
-  board[index]== "X" || board[index] == "O"
-end
- 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-end
-
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+def valid_move?(board, input)
+  input.to_i.between?(1,9) && !position_taken?(board, input_to_i-1)
 end
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
@@ -68,23 +49,44 @@ def draw?(board)
   !won?(board) && full?(board)
 end
 
- def over?(board)
-  won?(board) || draw?(board)
+def over?(board)
+ won?(board) || draw?(board)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if !valid_move?(board, input)
+    turn(board)
+  end
+  move(board, input, current_player(board))
+  display_board(board)
+end
+
+def input_to_index(user_input)
+  converted_input = "#{user_input}".to_i - 1	   user_input.to_i - 1
+end	end
+
+def position_taken?(board, location)
+  !(board[location].nil? || board[location] == " ")
+  # Creates a stop on RSpec
+  # !(board[location].nil? || board[location] == "")
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def move(board, location, player = "X")
+  board[location.to_i-1] = player
 end
 
 def winner(board)
   if winning_combo = won?(board)
     board[winning_combo.first]
-  end
-end
-
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-   if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
   end
 end
